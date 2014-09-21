74293ddb8befd0ce00ed81dfbf5627e38d26d6fd
suisho <suisho000000@gmail.com>
2014-09-22 08:37:38 +0900

    add

---
+++ MyTableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  MyTableViewController.swift
+//  TableStretch
+//
+//  Created by suisho on 2014/09/21.
+//  Copyright (c) 2014年 suisho. All rights reserved.
+//
+
+import UIKit
+class Documents{
+    var body: [String] = [
+        "あああああああああああああああああああああああああああ",
+        "いいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいいい",
+        "ううう"
+    ]
+}
+class MyTableViewController: UITableViewController {
+    
+    let doc = Documents()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return doc.body.count + 1
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+        let cell = UITableViewCell()
+        cell.textLabel?.numberOfLines = 0
+        // Configure the cell...
+        //println( doc.body[indexPath.row])
+        cell.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+
+        if indexPath.row < 3{
+            cell.textLabel?.text = doc.body[indexPath.row]
+        }else{
+            let img = UIImage(named: "scratch.jpg")
+            cell.imageView?.image = img
+        }
+        
+        return cell
+    }
+    
+
+
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TableStretch/MyPlayground.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Park{
+    var dogs: [String] = [String]()
+}
+extension Park{
+    
+}
+class Mario{
+    init(){
+        var p = Park()
+        p.dogs.append("a")
+    }
+}
+
+Mario()
